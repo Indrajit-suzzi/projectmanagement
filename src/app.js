1e32ca6bb43e0bc912e4c925092159fa0770bf59
Indrajit-Zorin <indrajitroy8536@gmail.com>
2026-04-24 09:34:46 +0530

    api error and respponse configuration added, mongodb connected

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// Basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// Cors configurtions
+app.use(
+  cors({
+    origin: process.env.CORS_ERIGIN?.split(",") || "http://localhost:5173",
+    Credential: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("welcome to basecampy");
