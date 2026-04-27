93a278721c80282e096ccbc8e7d1578ff160fca3
Indrajit-Zorin <indrajitroy8536@gmail.com>
2026-04-27 23:15:28 +0530

    feat: update auth routes and add validators

---
+++ src/middlewares/validator.moddleware.js
@@ -0,0 +1,15 @@
+import { validationResult } from "express-validator";
+import { ApiError } from "../utils/api-error.js";
+
+export const validate = (req, res, next) => {
+  const error = validationResult(req);
+
+  if (error.isEmpty()) {
+    return next();
+  }
+
+  const extractedError = [];
+  error.array().map((err) => extractedError.push({ [err.path]: err.msg }));
+
+  throw new ApiError(422, "Recived data is not valid", extractedError);
+};
